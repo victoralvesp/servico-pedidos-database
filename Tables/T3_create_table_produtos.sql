9006fe5fccb69bb0aa413d488dd961d16abb33fb
Victor Alves <vpalves1@gmail.com>
2018-12-07 19:44:35 -0200

    Pequenas correções nos scripts de criação

--- Tables/T3_create_table_produtos.sql
+++ Tables/T3_create_table_produtos.sql
@@ -14,7 +14,7 @@ CREATE TABLE dbo.Produtos
     id INT NOT NULL PRIMARY KEY, -- primary key column
     nome VARCHAR(50) NOT NULL,
     preco_sugerido DECIMAL(18,2) NOT NULL,
-    preco_sugerido_moeda VARHCHAR(3) NOT NULL,
+    preco_sugerido_moeda VARCHAR(3) NOT NULL,
     multiplo INT
 );
 
--- create_database.sql
+++ create_database.sql
@@ -10,18 +10,3 @@ IF NOT EXISTS (
 )
 CREATE DATABASE Pedidos
 
-USE Pedidos
-
--- Create a new table called 'VersaoScripts' in schema 'dbo'
--- Drop the table if it already exists
-IF OBJECT_ID('dbo.VersaoScripts', 'U') IS NOT NULL
-DROP TABLE dbo.VersaoScripts
-
--- Create the table in the specified schema
-CREATE TABLE dbo.VersaoScripts
-(
-    id INT IDENTITY PRIMARY KEY, -- primary key column
-    script VARCHAR(5) NOT NULL,
-    data_execucao DATETIME NOT NULL
-    -- specify more columns here
-);
